deca99e80fa78f1224eaef85c0f014c6480056fe
Cainã <caina.wanzeler@gmail.com>
2025-11-30 14:46:41 -0300

    update ghPages

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ghPages from 'vite-plugin-gh-pages'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
+    ghPages(),
     vueDevTools(),
   ],
   resolve: {
@@ -15,4 +18,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: '/curriculo-gaby/'
 })
